55f75cd4b0f320963368a766f25a11eb29543f7e
tomfin <tom.finlayson@gmail.com>
2015-09-01 11:22:38 +0100

    Commented out file - not in use anyway...

--- app/scripts/contact.js
+++ app/scripts/contact.js
@@ -1,8 +1,9 @@
 /* Contact Form Script */
 
+/*
 (function() {
 
-	"use strict";
+	'use strict';
 
 	var contactForm = {
 
@@ -10,7 +11,7 @@
 
 		initialize: function() {
 
-			if (this.initialized) return;
+			if (this.initialized) { return; }
 			this.initialized = true;
 
 			this.build();
@@ -30,64 +31,64 @@
 
 		validations: function() {
 
-			var contactform = $("#contact-form"),
-				url = contactform.attr("action");
+			var contactform = $('#contact-form'), //jshint ignore:line
+				url = contactform.attr('action');
 
 			contactform.validate({
-				submitHandler: function(form) {
+				submitHandler: function(form) { //jshint ignore:line
 
 					// Loading State
-					var submitButton = $(this.submitButton);
-					submitButton.button("loading");
+					var submitButton = $(this.submitButton); //jshint ignore:line
+					submitButton.button('loading');
 
 					// Ajax Submit
-					$.ajax({
-						type: "POST",
+					$.ajax({ //jshint ignore:line
+						type: 'POST',
 						url: url,
 						data: {
-							"name": $("#contact-form #name").val(),
-							"email": $("#contact-form #email").val(),
-							"url": $("#contact-form #url").val(),
-							"message": $("#contact-form #message").val()
+							'name': $('#contact-form #name').val(), //jshint ignore:line
+							'email': $('#contact-form #email').val(), //jshint ignore:line
+							'url': $('#contact-form #url').val(), //jshint ignore:line
+							'message': $('#contact-form #message').val() //jshint ignore:line
 						},
-						dataType: "json",
+						dataType: 'json',
 						success: function (data) {
-							if (data.response == "success") {
+							if (data.response === 'success') {
 
-								$("#contact-alert-success").removeClass("hidden");
-								$("#contact-alert-error").addClass("hidden");
+								$('#contact-alert-success').removeClass('hidden'); //jshint ignore:line
+								$('#contact-alert-error').addClass('hidden'); //jshint ignore:line
 
 								// Reset Form
-								$("#contact-form .form-control")
-									.val("")
+								$('#contact-form .form-control') //jshint ignore:line
+									.val('')
 									.blur()
 									.parent()
-									.removeClass("has-success")
-									.removeClass("has-error")
-									.find("label.error")
+									.removeClass('has-success')
+									.removeClass('has-error')
+									.find('label.error')
 									.remove();
 
-								if(($("#contact-alert-success").position().top - 80) < $(window).scrollTop()){
-									$("html, body").animate({
-										 scrollTop: $("#contact-alert-success").offset().top - 80
+								if(($('#contact-alert-success').position().top - 80) < $(window).scrollTop()){ //jshint ignore:line
+									$('html, body').animate({ //jshint ignore:line
+										 scrollTop: $('#contact-alert-success').offset().top - 80 //jshint ignore:line
 									}, 300);
 								}
 
 							} else {
 
-								$("#contact-alert-error").removeClass("hidden");
-								$("#contact-alert-success").addClass("hidden");
+								$('#contact-alert-error').removeClass('hidden'); //jshint ignore:line
+								$('#contact-alert-success').addClass('hidden'); //jshint ignore:line
 
-								if(($("#contact-alert-error").position().top - 80) < $(window).scrollTop()){
-									$("html, body").animate({
-										scrollTop: $("#contact-alert-error").offset().top - 80
+								if(($('#contact-alert-error').position().top - 80) < $(window).scrollTop()){ //jshint ignore:line
+									$('html, body').animate({ //jshint ignore:line
+										scrollTop: $('#contact-alert-error').offset().top - 80 //jshint ignore:line
 									}, 300);
 								}
 
 							}
 						},
 						complete: function () {
-							submitButton.button("reset");
+							submitButton.button('reset');
 						}
 					});
 				},
@@ -107,19 +108,19 @@
 					}
 				},
 				highlight: function (element) {
-					$(element)
+					$(element) //jshint ignore:line
 						.parent()
-						.removeClass("has-success")
-						.addClass("has-error");
+						.removeClass('has-success')
+						.addClass('has-error');
 				},
 				success: function (element) {
-					$(element)
+					$(element) //jshint ignore:line
 						.parent()
-						.removeClass("has-error")
-						.addClass("has-success")
-						.find("label.error")
+						.removeClass('has-error')
+						.addClass('has-success')
+						.find('label.error')
 						.remove();
-				},
+				}
 			});
 
 		}
@@ -129,3 +130,4 @@
 	contactForm.initialize();
 
 })();
+*/
