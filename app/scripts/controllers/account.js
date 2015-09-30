f93667b92f780b83fa8e191a8c20340ff0b20577
Parthvora <parth@ossquad.com>
2015-09-30 13:24:03 +0530

    controller for account page

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -9,11 +9,39 @@
  */
 angular.module('fastrankApp')
 .controller('AccountCtrl', [ '$scope', '$log', 'Principal', function ($scope, $log, Principal) {
-    var acc = Principal.identity();
-    $scope.account = acc.$$state.value;
-    $scope.account_update = function () {
-      $log.info($scope.account);
-    };
+  var acc = Principal.identity();
+  $scope.account = acc.$$state.value;
+  $log.info($scope.account);
+  $scope.doNotMatch = null;
+  $scope.noCurrentPsw = null;
+  
+  $scope.account_update = function () {   
+    //$log.info($scope.account);
+  };
+  
+  $scope.change_password = function() {
+    if(angular.isDefined($scope.account.newPassword) && angular.isDefined($scope.account.confirmPassword)) {
+      if($scope.account.newPassword === $scope.account.confirmPassword) {
+        $scope.doNotMatch = null;
+        if(angular.isDefined($scope.account.currentPassword)) {
+          $scope.noCurrentPsw = null;
+        } else {
+          $scope.noCurrentPsw = 'ERROR';
+        }
+      } else {
+        $scope.noCurrentPsw = null;
+        $scope.doNotMatch = 'ERROR';
+      }
+    } else if (angular.isDefined($scope.account.newPassword) && !angular.isDefined($scope.account.confirmPassword)
+           || !angular.isDefined($scope.account.newPassword) && angular.isDefined($scope.account.confirmPassword)) {
+      $scope.noCurrentPsw = null;
+      $scope.doNotMatch = 'ERROR';
+    } else {
+      $scope.noCurrentPsw = null;
+      $scope.doNotMatch = null;
+    }
+  }
+  
 }]);
   
 
