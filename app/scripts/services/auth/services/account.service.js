c90b397fe4ee7c4d3eb2479931992c6a87f112be
tomfin <tom.finlayson@gmail.com>
2015-10-15 10:59:45 +0100

    Modified to use public account endpoint.

--- app/scripts/services/auth/services/account.service.js
+++ app/scripts/services/auth/services/account.service.js
@@ -2,7 +2,7 @@
 
 angular.module('fastrankApp')
     .factory('Account', function Account($resource, ENDPOINT) {
-        return $resource(ENDPOINT + 'api/account', {}, {
+        return $resource(ENDPOINT + 'api/public/account', {}, {
             'get': { method: 'GET', params: {}, isArray: false,
                 interceptor: {
                     response: function(response) {
